7a5ac1e66f158ecd81b23d8d61b549a86e6f44e8
김대웅 <dkssud9556@dsm.hs.kr>
2021-02-08 22:10:55 +0900

    Add project base

---
+++ src/main/java/com/dsmupgrade/DsmUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.dsmupgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmUpgradeApplication.class, args);
+    }
+
+}
