2185c55ecc0d9f2128f61c737a8849c96e8992b1
김대웅 <dkssud9556@dsm.hs.kr>
2021-02-10 00:03:48 +0900

    Add 500 internal server error code

--- src/main/java/com/dsmupgrade/global/error/exception/ErrorCode.java
+++ src/main/java/com/dsmupgrade/global/error/exception/ErrorCode.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    INVALID_INPUT_VALUE(400, "C001", "Invalid Input Value"),
+    INTERNAL_SERVER_ERROR(500, "C001", "Internal Server Error"),
+    INVALID_INPUT_VALUE(400, "C002", "Invalid Input Value"),
 
     FIELD_NOT_FOUND(404, "F001", "Field Not Found");
 
     private final String code;
     private final String message;
-    private int status;
+    private final int status;
 
     ErrorCode(final int status, final String code, final String message) {
         this.status = status;
