28029d58e90baa31fabaabd2a24a30936d8df52a
김대웅 <dkssud9556@dsm.hs.kr>
2021-02-09 23:45:11 +0900

    Add field not found exception

---
+++ src/main/java/com/dsmupgrade/exception/FieldNotFoundException.java
@@ -0,0 +1,11 @@
+package com.dsmupgrade.exception;
+
+import com.dsmupgrade.global.error.exception.BusinessException;
+import com.dsmupgrade.global.error.exception.ErrorCode;
+
+public class FieldNotFoundException extends BusinessException {
+
+    public FieldNotFoundException(int id) {
+        super("field id " + id + " is not found", ErrorCode.FIELD_NOT_FOUND);
+    }
+}
