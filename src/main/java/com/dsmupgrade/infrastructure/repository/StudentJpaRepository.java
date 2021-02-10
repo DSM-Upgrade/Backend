0c92d050397fb85c9a426e4711d17037759f2b50
김대웅 <dkssud9556@dsm.hs.kr>
2021-02-10 22:17:50 +0900

    Add findByUsername in student repository

--- src/main/java/com/dsmupgrade/infrastructure/repository/StudentJpaRepository.java
+++ src/main/java/com/dsmupgrade/infrastructure/repository/StudentJpaRepository.java
@@ -4,8 +4,13 @@ import com.dsmupgrade.domain.entity.Student;
 import com.dsmupgrade.domain.repository.StudentRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentJpaRepository extends StudentRepository, JpaRepository<Student, String> {
 
     @Override
     Student save(Student student);
+
+    @Override
+    Optional<Student> findByUsername(String username);
 }
