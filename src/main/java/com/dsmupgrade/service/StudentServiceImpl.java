4f030e32179537c026d10564619957467180cd8f
김대웅 <03rlaeodnd@gmail.com>
2021-02-15 23:07:02 +0900

    Add update password service

--- src/main/java/com/dsmupgrade/service/StudentServiceImpl.java
+++ src/main/java/com/dsmupgrade/service/StudentServiceImpl.java
@@ -2,9 +2,12 @@ package com.dsmupgrade.service;
 
 import com.dsmupgrade.domain.entity.Student;
 import com.dsmupgrade.domain.repository.StudentRepository;
+import com.dsmupgrade.dto.request.PasswordRequest;
 import com.dsmupgrade.dto.response.StudentResponse;
+import com.dsmupgrade.global.error.exception.PasswordNotMatchedException;
 import com.dsmupgrade.global.error.exception.StudentNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public StudentResponse getStudentByUsername(String username) {
@@ -19,5 +23,18 @@ public class StudentServiceImpl implements StudentService {
                 .orElseThrow(() -> new StudentNotFoundException(username));
 
         return StudentResponse.of(student);
+    }
+
+    @Override
+    public void updateStudentPassword(String username, PasswordRequest passwordRequest) {
+        Student student = studentRepository.findByUsername(username)
+                .orElseThrow(() -> new StudentNotFoundException(username));
+
+        if (!passwordEncoder.matches(passwordRequest.getNowPassword(), student.getPassword())) {
+            throw new PasswordNotMatchedException();
+        }
+
+        student.updatePassword(passwordRequest.getNewPassword());
+        studentRepository.save(student);
     }
 }
