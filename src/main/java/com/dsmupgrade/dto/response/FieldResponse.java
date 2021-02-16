af6daf797e1fde9d52ec5f7e90ed3c2be720816d
김대웅 <03rlaeodnd@gmail.com>
2021-02-16 17:31:14 +0900

    Add field response dto

---
+++ src/main/java/com/dsmupgrade/dto/response/FieldResponse.java
@@ -0,0 +1,24 @@
+package com.dsmupgrade.dto.response;
+
+import com.dsmupgrade.domain.entity.Field;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+public class FieldResponse {
+
+    private final int id;
+
+    private final String name;
+
+    public static FieldResponse of(Field field) {
+        return FieldResponse.builder()
+                .id(field.getId())
+                .name(field.getName())
+                .build();
+    }
+}
