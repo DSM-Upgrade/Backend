5e6055fa83f2091aacd9d7679932a122d144150a
김대웅 <03rlaeodnd@gmail.com>
2021-02-16 17:31:22 +0900

    Add common service

---
+++ src/main/java/com/dsmupgrade/service/CommonService.java
@@ -0,0 +1,10 @@
+package com.dsmupgrade.service;
+
+import com.dsmupgrade.dto.response.FieldResponse;
+
+import java.util.List;
+
+public interface CommonService {
+
+    List<FieldResponse> getAllFields();
+}
