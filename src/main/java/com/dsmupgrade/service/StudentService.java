b7a8933ff8d28d0d28543bf798555764d2bd0aa0
김대웅 <03rlaeodnd@gmail.com>
2021-02-16 23:07:48 +0900

    Add update student service

--- src/main/java/com/dsmupgrade/service/StudentService.java
+++ src/main/java/com/dsmupgrade/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.dsmupgrade.service;
 
 import com.dsmupgrade.dto.request.PasswordRequest;
+import com.dsmupgrade.dto.request.UpdateStudentRequest;
 import com.dsmupgrade.dto.response.StudentResponse;
 
 public interface StudentService {
@@ -8,4 +9,6 @@ public interface StudentService {
     StudentResponse getStudentByUsername(String username);
 
     void updateStudentPassword(String username, PasswordRequest passwordRequest);
+
+    void updateStudentInfo(String username, UpdateStudentRequest updateStudentRequest);
 }
--- src/main/java/com/dsmupgrade/service/StudentServiceImpl.java
+++ src/main/java/com/dsmupgrade/service/StudentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.dsmupgrade.service;
 
+import com.dsmupgrade.domain.entity.Field;
 import com.dsmupgrade.domain.entity.Student;
+import com.dsmupgrade.domain.repository.FieldRepository;
 import com.dsmupgrade.domain.repository.StudentRepository;
 import com.dsmupgrade.dto.request.PasswordRequest;
+import com.dsmupgrade.dto.request.UpdateStudentRequest;
 import com.dsmupgrade.dto.response.StudentResponse;
+import com.dsmupgrade.global.error.exception.FieldNotFoundException;
 import com.dsmupgrade.global.error.exception.PasswordNotMatchedException;
 import com.dsmupgrade.global.error.exception.StudentNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
+    private final FieldRepository fieldRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -35,6 +40,27 @@ public class StudentServiceImpl implements StudentService {
         }
 
         student.updatePassword(passwordRequest.getNewPassword());
+        studentRepository.save(student);
+    }
+
+    @Override
+    public void updateStudentInfo(String username, UpdateStudentRequest updateStudentRequest) {
+        Student student = studentRepository.findByUsername(username)
+                .orElseThrow(() -> new StudentNotFoundException(username));
+
+        updateStudentRequest.nullCheck();
+
+        if (updateStudentRequest.getStudentNum() != null) {
+            student.updateStudentNum(updateStudentRequest.getStudentNum());
+        }
+
+        if (updateStudentRequest.getFieldId() != null) {
+            Field field = fieldRepository.findById(updateStudentRequest.getFieldId())
+                    .orElseThrow(() -> new FieldNotFoundException(updateStudentRequest.getFieldId()));
+
+            student.updateField(field);
+        }
+
         studentRepository.save(student);
     }
 }
