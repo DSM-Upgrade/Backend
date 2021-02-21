320734399fff1aae2d34d919f4c095645d70209f
jun <toinin@naver.com>
2021-02-22 03:05:33 +0900

    Add authority

--- src/main/java/com/dsmupgrade/service/authority/AuthorityServiceImpl.java
+++ src/main/java/com/dsmupgrade/service/authority/AuthorityServiceImpl.java
@@ -7,18 +7,26 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthorityServiceImpl implements AuthorityService {
+//    private final StudentRepository studentRepository;
+
     @Override
     public void AuthorityHomework(AdminAuthRequest adminAuthRequest) {
-        
+//        String username = AuthenticationFacade.getUsername();
+//        Student student = studentRepository.findByUsername(username);
+//        studentRepository.save(student.fineManager());
     }
 
     @Override
     public void AuthorityFine(AdminAuthRequest adminAuthRequest) {
-
+//        String username = AuthenticationFacade.getUsername();
+//        Student student = studentRepository.findByUsername(username);
+//        studentRepository.save(student.noticeManage());
     }
 
     @Override
-    public void AdminAuth(AdminAuthRequest adminAuthRequest){
-
+    public void AdminAuth(AdminAuthRequest adminAuthRequest) {
+//        String username = AuthenticationFacade.getUsername();
+//        Student student = studentRepository.findByUsername(username);
+//        studentRepository.save(student.register());
     }
 }
