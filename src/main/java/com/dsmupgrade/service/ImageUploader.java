dfe13ac06687d2a71cc5e1d19f51edd0dab0dd3c
김대웅 <dkssud9556@dsm.hs.kr>
2021-02-23 23:27:55 +0900

    Add s3 image uploader

--- src/main/java/com/dsmupgrade/service/ImageUploader.java
+++ src/main/java/com/dsmupgrade/service/ImageUploader.java
@@ -1,4 +1,10 @@
 package com.dsmupgrade.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface ImageUploader {
+
+    String upload(String username, MultipartFile multipartFile, String dir) throws IOException;
 }
