9a7e0b465edee68f3873593eccee29dba727ff6a
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-02 15:43:43 +0900

    Add token refresh logic

--- src/main/java/com/dsmupgrade/service/AuthService.java
+++ src/main/java/com/dsmupgrade/service/AuthService.java
@@ -3,12 +3,13 @@ package com.dsmupgrade.service;
 import com.dsmupgrade.dto.request.LoginRequest;
 import com.dsmupgrade.dto.request.SignUpRequest;
 import com.dsmupgrade.dto.response.LoginResponse;
+import com.dsmupgrade.dto.response.TokenRefreshResponse;
 
 public interface AuthService {
 
     void signUp(SignUpRequest signUpRequest);
 
     LoginResponse login(LoginRequest loginRequest);
-    
-    String generateNewToken(String refreshToken);
+
+    TokenRefreshResponse generateNewToken(String refreshToken);
 }
