84359c9cd348e90dd8b01e712f7b9004ea0c0aec
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-18 10:58:00 +0900

    Add repository annotation

--- src/main/java/com/dsmupgrade/domain/student/infrastructure/repository/StudentJpaRepository.java
+++ src/main/java/com/dsmupgrade/domain/student/infrastructure/repository/StudentJpaRepository.java
@@ -3,8 +3,8 @@ package com.dsmupgrade.domain.student.infrastructure.repository;
 import com.dsmupgrade.domain.student.domain.Student;
 import com.dsmupgrade.domain.student.domain.StudentRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface StudentJpaRepository extends StudentRepository, JpaRepository<Student, String> {
 }
