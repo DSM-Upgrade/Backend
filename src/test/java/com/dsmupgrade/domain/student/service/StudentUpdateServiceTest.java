f8154e28ce550be48a5fabd73e6276b29d2a5c40
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-22 09:44:10 +0900

    [ADD] 프로필 변경 테스트

--- src/test/java/com/dsmupgrade/domain/student/service/StudentUpdateServiceTest.java
+++ src/test/java/com/dsmupgrade/domain/student/service/StudentUpdateServiceTest.java
@@ -4,15 +4,25 @@ import com.dsmupgrade.domain.student.dto.request.PasswordRequest;
 import com.dsmupgrade.global.error.exception.StudentNotFoundException;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 public class StudentUpdateServiceTest extends StudentServiceTest {
 
     @InjectMocks
     private StudentUpdateService studentUpdateService;
+
+    @MockBean
+    private ImageUploader imageUploader;
 
     @Test(expected = StudentNotFoundException.class)
     public void 비밀번호_변경_학생없음() {
@@ -24,5 +34,64 @@ public class StudentUpdateServiceTest extends StudentServiceTest {
 
         //when
         studentUpdateService.updateStudentPassword(username, dto);
+    }
+
+    @Test
+    public void 프로필_이미지_변경() throws IOException {
+        //given
+        String username = student.getUsername();
+        MultipartFile file = createEmptyMultipartFile();
+
+        given(imageUploader.upload(username, any(), any())).willReturn(username);
+
+        //when
+        String uploadedFileName = studentUpdateService.updateStudentProfile(username, file);
+
+        //then
+        assertThat(uploadedFileName).isEqualTo(username);
+    }
+
+    private MultipartFile createEmptyMultipartFile() {
+        return new MultipartFile() {
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public String getOriginalFilename() {
+                return null;
+            }
+
+            @Override
+            public String getContentType() {
+                return null;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public long getSize() {
+                return 0;
+            }
+
+            @Override
+            public byte[] getBytes() throws IOException {
+                return new byte[0];
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return null;
+            }
+
+            @Override
+            public void transferTo(File dest) throws IOException, IllegalStateException {
+
+            }
+        };
     }
 }
