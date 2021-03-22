e6900324899468424b8e1663111675050fd9a60e
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-22 10:21:18 +0900

    [ADD] @Ignore 어노테이션

--- src/test/java/com/dsmupgrade/domain/student/service/ImageUploaderTest.java
+++ src/test/java/com/dsmupgrade/domain/student/service/ImageUploaderTest.java
@@ -1,6 +1,7 @@
 package com.dsmupgrade.domain.student.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,6 +12,7 @@ import java.net.URL;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+@Ignore
 public class ImageUploaderTest extends StudentServiceTest {
 
     @InjectMocks
