64e49067fc9ca9b9021bc5e1fa86504b0c0b5e63
dohyeon5626 <kodohyeon71@gmail.com>
2021-07-14 20:22:56 +0900

    [PATCH] dto value

--- src/main/java/com/dsmupgrade/domain/homework/dto/response/HomeworkContentAdminResponse.java
+++ src/main/java/com/dsmupgrade/domain/homework/dto/response/HomeworkContentAdminResponse.java
@@ -1,37 +1,18 @@
 package com.dsmupgrade.domain.homework.dto.response;
 
 import com.dsmupgrade.domain.homework.domain.Homework;
-import com.dsmupgrade.domain.homework.domain.HomeworkFile;
 import com.dsmupgrade.domain.homework.domain.PersonalHomework;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
-import org.springframework.util.CollectionUtils;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(access = AccessLevel.PRIVATE)
 public class HomeworkContentAdminResponse {
-    private String title;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
-    private LocalDateTime createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
-    private LocalDateTime deadline;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
-    private LocalDateTime returnAt;
     private String content;
 
     public static HomeworkContentAdminResponse from(PersonalHomework personalHomework) {
-        Homework homework = personalHomework.getHomework();
         return HomeworkContentAdminResponse.builder()
-                .title(homework.getTitle())
-                .createdAt(homework.getCreatedAt())
-                .deadline(homework.getDeadline())
-                .returnAt(personalHomework.getSubmittedAt())
                 .content(personalHomework.getContent())
                 .build();
     }
